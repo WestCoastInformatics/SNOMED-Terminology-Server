ee5d5ed72a0b5bd5e6306e27955b1d9887b3652f
Deborah Shapiro <deborah.seider.shapiro@gmail.com>
2015-03-26 16:04:05 -0700

    Initial work for ProjectServiceRest Integration tests.

--- integration-test/src/test/java/org/ihtsdo/otf/ts/test/rest/ProjectServiceRestNormalUseTest.java
+++ integration-test/src/test/java/org/ihtsdo/otf/ts/test/rest/ProjectServiceRestNormalUseTest.java
@@ -3,12 +3,22 @@
  */
 package org.ihtsdo.otf.ts.test.rest;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Set;
 
 import org.ihtsdo.otf.ts.Project;
 import org.ihtsdo.otf.ts.User;
+import org.ihtsdo.otf.ts.classifier.model.Relationship;
+import org.ihtsdo.otf.ts.helpers.PfsParameter;
+import org.ihtsdo.otf.ts.helpers.PfsParameterJpa;
+import org.ihtsdo.otf.ts.helpers.ProjectList;
+import org.ihtsdo.otf.ts.helpers.SearchResult;
+import org.ihtsdo.otf.ts.helpers.SearchResultList;
 import org.ihtsdo.otf.ts.jpa.ProjectJpa;
+import org.ihtsdo.otf.ts.rf2.Concept;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -41,7 +51,7 @@ public class ProjectServiceRestNormalUseTest extends ProjectServiceRestTest {
   }
 
   /**
-   * Test Get and Find methods for concepts.
+   * Test get, update, and remove project.
    *
    * @throws Exception the exception
    */
@@ -91,13 +101,53 @@ public class ProjectServiceRestNormalUseTest extends ProjectServiceRestTest {
   }
 
   /**
-   * Test transitive closure methods (ancestors, descendants, children).
+   * Test getProjects()
    *
    * @throws Exception the exception
    */
   @Test
   public void testNormalUseRestProject002() throws Exception {
-    //
+	// Add a project
+	    ProjectJpa project = new ProjectJpa();
+	    Set<String> values = new HashSet<>();
+	    values.add("PUBLISHED");
+	    project.setActionWorkflowStatusValues(values);
+	    User user = securityService.getUser(adminUser, adminAuthToken);
+	    project.addAdministrator(user);
+	    project.addAuthor(user);
+	    project.addLead(user);
+	    project.addScopeConcept("12345");
+	    project.addScopeExcludesConcept("12345");
+	    project.setDescription("Sample");
+	    project.setModuleId("12345");
+	    project.setName("Sample");
+	    project.setTerminology("SNOMEDCT");
+	    project.setTerminologyVersion("latest");
+	    projectService.addProject(project, adminAuthToken);
+	    
+	 // Add a second project
+	    ProjectJpa project2 = new ProjectJpa(project);
+	    project2.setName("Sample 2");
+	    project2.setDescription("Sample 2");
+	    projectService.addProject(project2, adminAuthToken);
+	    
+	 // Get the projects
+	    ProjectList projectList = projectService.getProjects(adminAuthToken);
+	    int projectCount = projectList.getCount();
+	    Assert.assertTrue(projectList.contains(project));
+	    Assert.assertTrue(projectList.contains(project2));
+	    
+	 // remove first project
+	    projectService.removeProject(project.getId(), adminAuthToken);
+	    projectList = projectService.getProjects(adminAuthToken);
+	    Assert.assertTrue(projectList.getCount() == projectCount - 1);
+	    
+	 // remove second project
+	    projectService.removeProject(project.getId(), adminAuthToken);
+	    projectList = projectService.getProjects(adminAuthToken);
+	    Assert.assertTrue(projectList.getCount() == projectCount - 2); 
+	    
+	    
   }
 
   /**
@@ -107,7 +157,33 @@ public class ProjectServiceRestNormalUseTest extends ProjectServiceRestTest {
    */
   @Test
   public void testNormalUseRestProject003() throws Exception {
-    //
+	// Get the projects
+	    ProjectList projectList = projectService.getProjects(viewerAuthToken);
+	    Assert.assertTrue(projectList.getCount() == 1);
+	    Assert.assertTrue(projectList.getObjects().get(0).getName().equals("Sample Project"));
+	    
+	    Set<String> scopeConcepts = projectList.getObjects().get(0).getScopeConcepts();
+	    Assert.assertTrue(scopeConcepts.size() == 1);
+	    Assert.assertTrue(scopeConcepts.toArray()[0].toString().equals("138875005"));
+	    
+	// Call findConceptsInScope() pfs is null
+	    SearchResultList resultList = projectService.findConceptsInScope(
+	    		projectList.getObjects().get(0).getId(), null, viewerAuthToken);
+	    Assert.assertTrue(resultList.getCount() == 10293);
+	    
+	// Call findConceptsInScope() pfs gets first 10
+	    PfsParameterJpa pfs = new PfsParameterJpa();
+        pfs.setMaxResults(10);
+        resultList = projectService.findConceptsInScope(projectList.getObjects().get(0).getId(), pfs, viewerAuthToken);
+        Assert.assertTrue(resultList.getCount() == 10);
+        
+    // Make sure first 10 are sorted by dpn
+        Collections.sort(resultList.getObjects(), new Comparator<SearchResult>() {
+            @Override
+            public int compare(SearchResult o1, SearchResult o2) {
+              return o1.toString().compareTo(o2.toString());
+            }
+          });
   }
 
   /**
--- jpa-model/src/main/java/org/ihtsdo/otf/ts/jpa/ProjectJpa.java
+++ jpa-model/src/main/java/org/ihtsdo/otf/ts/jpa/ProjectJpa.java
@@ -183,7 +183,7 @@ public class ProjectJpa implements Project {
   @XmlID
   @Override
   public String getObjectId() {
-    return id.toString();
+    return (id == null ? "" : id.toString());
   }
 
 
